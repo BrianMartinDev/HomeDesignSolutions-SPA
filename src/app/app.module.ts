2fefd2cf47129f779e22c3cdfd7abd29d8da09d9
Brian Martin <46796447+BrianMartinDev@users.noreply.github.com>
2020-03-09 03:15:57 -0400

    added home components and styling files.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { FeaturedComponent } from './featured/featured.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    FeaturedComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
